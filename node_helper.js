a1c2052a432507328f7d1cfc34ab9c4413a3e717
Simon Wimmer <sw250@hdm-stuttgart.de>
2026-04-06 00:33:07 +0200

    Fix: handle printer status fetch errors by sending HTTP_ERROR notification

--- node_helper.js
+++ node_helper.js
@@ -29,7 +29,8 @@ module.exports = NodeHelper.create({
 
     const printer_status = await this.fetchPrinterStatus();
 
-    if (!printer_status) {
+    if (!printer_status || printer_status.error) {
+      this.sendSocketNotification("HTTP_ERROR", {});
       this.fetchTimerId = setTimeout(async function () {
         await self.fetchData();
       }, this.config.updateInterval);
